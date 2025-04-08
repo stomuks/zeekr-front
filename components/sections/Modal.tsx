44e2b1c8cdb742277bfd1a317e03c7202d5f712d
stormuks <stormuks1@gmail.com>
2025-04-08 17:57:01 +0300

    🔧 Fixed modal form

--- components/sections/Modal.tsx
+++ components/sections/Modal.tsx
@@ -21,7 +21,7 @@ export const Modal: FC<{
 			<DialogTrigger asChild>{children}</DialogTrigger>
 			<DialogPortal>
 				<DialogOverlay className='fixed inset-0 bg-gray-800 opacity-45 data-[state=open]:animate-overlayShow' />
-				<DialogContent className='p-2 md:p-10 rounded-xl min-w-[80vw] max-w-[95vw] 2xl:min-w-[1440px] bg-gray-100 lg:rounded-[45px] overflow-y-auto max-h-[95vh]'>
+				<DialogContent className='p-2 md:p-10 rounded-xl min-w-[80vw] max-w-[95vw] 2xl:min-w-[1440px] 2xl:max-w-[1440px] bg-gray-100 lg:rounded-[45px] overflow-y-auto max-h-[95vh]'>
 					<DialogHeader>
 						<DialogTitle className='hidden'>Форма обратной связи</DialogTitle>
 						<DialogDescription className='hidden'>
@@ -32,7 +32,7 @@ export const Modal: FC<{
 						<div className='rounded-xl md:rounded-l-[45px]'>
 							<MyForm />
 						</div>
-						<div className='relative 2xl:min-h-[856px] h-[50vw]'>
+						<div className='relative 2xl:h-full h-[50vw]'>
 							<Image
 								fill
 								src={image ?? '/Zeekr/Zeekr001/5.avif'}
